b992b7eae1e07afb11d80a0f37cb9bc7c6fb1c8b
honour112 <keblehonour@gmail.com>
2025-11-12 11:03:06 +0100

    new

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -46,7 +46,7 @@ const Home: React.FC = () => {
           className="absolute inset-0 bg-cover bg-center"
           style={{
             backgroundImage:
-              "url('/public/assets/WhatsApp Image 2025-11-12 at 08.07.17_f02ed693.jpg')",
+              "url('/public/WhatsApp Image 2025-11-12 at 08.07.17_f02ed693.jpg')",
           }}
         />
         <div className="absolute inset-0 bg-dark-navy/85" />
